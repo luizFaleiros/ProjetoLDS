3973ec46ac05fe06bd65e282400a2349689ea640
luiz faleiros <luizhesafa21@gmail.com>
2020-12-17 22:03:16 -0300

    iniciando projeto

---
+++ src/test/java/br/com/projeto/LDS/LdsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.LDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
