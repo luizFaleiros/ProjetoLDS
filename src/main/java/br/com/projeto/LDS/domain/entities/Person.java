a3b2265bf5927e6f7b9f40e0b8611077359ea1c2
Luiz Faleiros <luiz.faleiros@iteris.com.br>
2020-12-20 12:18:41 -0300

    Colocando a entidade

--- src/main/java/br/com/projeto/LDS/domain/entities/Person.java
+++ src/main/java/br/com/projeto/LDS/domain/entities/Person.java
@@ -5,14 +5,23 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
+@Table(name = "PERSON")
 public class Person implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
